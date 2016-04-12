7213fc0c1c97de18252538898ae368dd9e6606af
David Rosillo <drr538@gmail.com>
2016-04-12 12:22:05 -0500

    Added paperclip attachment to Teaching

--- app/controllers/teachings_controller.rb
+++ app/controllers/teachings_controller.rb
@@ -70,6 +70,6 @@ class TeachingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def teaching_params
-      params.require(:teaching).permit(:teaching_name, :summary, :body, :user_id)
+      params.require(:teaching).permit(:document, :teaching_name, :summary, :body, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411144220) do
+ActiveRecord::Schema.define(version: 20160411211714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,12 @@ ActiveRecord::Schema.define(version: 20160411144220) do
     t.text     "summary"
     t.text     "body"
     t.integer  "user_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "document_file_name"
+    t.string   "document_content_type"
+    t.integer  "document_file_size"
+    t.datetime "document_updated_at"
   end
 
   add_index "teachings", ["user_id"], name: "index_teachings_on_user_id", using: :btree
