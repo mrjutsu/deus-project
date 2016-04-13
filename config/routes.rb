53e461d4720cdf1497f0215fd97e9be72cfdf8b0
David Rosillo <drr538@gmail.com>
2016-04-12 19:05:24 -0500

    Fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,10 @@ Rails.application.routes.draw do
     get :change_password, to: 'registrations#change_password'
     put :change_password, to: 'registrations#update_password'
 
-    # member do
-    #   get :profile, to: 'profile#show'
-    # end
-
     root to: 'teachings#index'
   end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations_controller" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
