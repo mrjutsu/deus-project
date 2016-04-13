b2e69c6cf93f559c8751257fa0ef819343c9c067
David Rosillo <drr538@gmail.com>
2016-04-12 19:05:59 -0500

    Added more information to Profile show view and added account update functionality

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,2 +1,11 @@
 module ProfileHelper
+
+  def user_full_name
+    if current_user.first_name.nil? && current_user.last_name.nil?
+      'Nombre no disponible'
+    else
+      current_user.first_name + ' ' + current_user.last_name
+    end
+  end
+
 end
