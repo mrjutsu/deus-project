7be32b54e3859fbf03f3d2a3dd0cf4459187c5f7
David Rosillo <drr538@gmail.com>
2016-04-14 11:03:52 -0500

    Added Mail confirmation to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413181417) do
+ActiveRecord::Schema.define(version: 20160414141128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,8 +90,12 @@ ActiveRecord::Schema.define(version: 20160413181417) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.integer  "role",                   default: 0
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
